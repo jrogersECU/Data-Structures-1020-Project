51c07ed9e3148e28c962024833f8ad0d4a04e790
saddHat’git config --global user.email rogersj22@students.ecu.edu’ <rogersj22@students.ecu.edu>
2022-12-04 14:22:35 -0500

    Removed redundant code and added cleaned up some user input requests.

--- main.py
+++ main.py
@@ -706,18 +706,17 @@ def begin_budget():
 
 
             if user_input == '5' or user_input.lower() == 'View Transaction Log':
-                #FixMe Add the code here for person 1
+
                 print(print(Person_1.print_logs()))
 
-                user_input = input()
+                user_input = input('Please press 1 to reset\n')
                 # reset program
                 if user_input == '1':
                     begin_budget()
 
             if user_input == '6' or user_input.lower() == 'Display info':
                 print(Person_1.print_all_info())
-                print("Press 1 to return to main program")
-                user_input = input()
+                user_input = input('Please press 1 to reset\n')
                 # reset program
                 if user_input == '1':
                     begin_budget()
@@ -1398,16 +1397,15 @@ def begin_budget():
                     begin_budget()
 
             if user_input == '5' or user_input.lower() == 'View Transaction Log':
-                #FixMe Add the code here for person 2
                 print(Person_2.print_logs())
-                user_input = input("Please press 1 to reset")
+                user_input = input("Please press 1 to reset\n")
                 # reset program
                 if user_input == '1':
                     begin_budget()
 
             if user_input == '6' or user_input.lower() == 'Display info':
                 print(Person_2.print_all_info())
-                user_input = input()
+                user_input = input('Please press 1 to reset\n')
                 # reset program
                 if user_input == '1':
                     begin_budget()
@@ -1424,48 +1422,6 @@ def begin_budget():
         if user_input == '1':
             begin_budget()
 
-
-
-
-
-
-#Person_1.add_pay_check(1000)
-# Deposit PayCheck
-#Person_1.add_pay_check(75)
-
-# Add Expense
-#Person_1.add_expense(200)
-# Add Expense
-
-#Person_1.add_expense(23)
-
-
-# Request Balance
-#Person_1.get_balance()
-
-# Request All User Data
-#Person_1.print_all_info()
-#Person_2.print_all_info()
-
-## Add Category Funds
-#Person_1.add_category_funds('Housing', 331)
-## Decrease Category Funds
-#Person_1.decrease_category_funds('Housing', 300)
-##Display Category Value
-#Person_1.print_category('Housing')
-##Create Additional Category
-#Person_1.add_custom_category('Dating', 100_000)
-##Transfer Category to Category Funds
-#Person_1.transfer_category_funds('Housing', 'Dating', 10_000)
-# Transfer All Category Funds
-#Person_1.transfer_all_category_funds('Housing', 'Dating')
-##Display All Person's Info
-#Person_1.print_all_info()
-##Display Person's Log
-#Person_1.print_logs()
-
-##Request Usage Percentage
-#Person_1.get_use_percentage()
 
 begin_budget()
 
